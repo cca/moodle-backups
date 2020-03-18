7b5a7bba3ed776769d50af2490c02b1253fbf6d3
phette23 <phette23@gmail.com>
2020-03-18 15:49:08 -0700

    add another csvsql cmd to get course IDs

--- createdb.sh
+++ createdb.sh
@@ -3,3 +3,5 @@
 csvsql --db sqlite:///courses.db --table courses --insert file.csv
 # add to already-existing database
 csvsql --db sqlite:///courses.db --table courses --no-create --insert file.csv
+# query existing spreadsheet, replace the semester code
+csvsql --query "SELECT * FROM '2016SU' WHERE Visibility = 'visible' OR Hits > 99" data/2016SU.csv
