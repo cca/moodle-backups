dbe15c4290cf66a1ac173fd4b58c57873e892d68
phette23 <phette23@gmail.com>
2020-03-20 15:49:39 -0700

    more specific csvkit code to get list of IDs

--- createdb.sh
+++ createdb.sh
@@ -3,5 +3,6 @@
 csvsql --db sqlite:///courses.db --table courses --insert file.csv
 # add to already-existing database
 csvsql --db sqlite:///courses.db --table courses --no-create --insert file.csv
-# query existing spreadsheet, replace the semester code
-csvsql --query "SELECT * FROM '2016SU' WHERE Visibility = 'visible' OR Hits > 99" data/2016SU.csv
+# query spreadsheet and get a list of IDs, can change the semester code here
+TERM="2018SP"
+csvsql --query "SELECT * FROM '${TERM}' WHERE Visibility = 'visible' OR Hits > 99" data/${TERM}.csv | csvcut -c 1 | sed 's/\.0//' > data/${TERM}.txt
