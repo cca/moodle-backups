99ee997f79a78c504d5b441778a2e6ff461898c0
phette23 <phette23@gmail.com>
2020-03-25 09:02:12 -0700

    TERM is a special env var, don't use it

--- createdb.sh
+++ createdb.sh
@@ -4,5 +4,5 @@ csvsql --db sqlite:///courses.db --table courses --insert file.csv
 # add to already-existing database
 csvsql --db sqlite:///courses.db --table courses --no-create --insert file.csv
 # query spreadsheet and get a list of IDs, can change the semester code here
-TERM="2018SP"
-csvsql --query "SELECT * FROM '${TERM}' WHERE Visibility = 'visible' OR Hits > 99" data/${TERM}.csv | csvcut -c 1 | sed 's/\.0//' > data/${TERM}.txt
+SEMESTER="2018SP"
+csvsql --query "SELECT * FROM '$SEMESTER' WHERE Visibility = 'visible' OR Hits > 99" data/$SEMESTER.csv | csvcut -c 1 | sed 's/\.0//' > data/$SEMESTER.txt
