2b8489937ff48e5a9b99a5bb6b0a4028be726e58
phette23 <phette23@gmail.com>
2020-03-25 09:18:53 -0700

    more tweaks to csvsql line to extract course IDs

--- createdb.sh
+++ createdb.sh
@@ -5,4 +5,4 @@ csvsql --db sqlite:///courses.db --table courses --insert file.csv
 csvsql --db sqlite:///courses.db --table courses --no-create --insert file.csv
 # query spreadsheet and get a list of IDs, can change the semester code here
 SEMESTER="2018SP"
-csvsql --query "SELECT * FROM '$SEMESTER' WHERE Visibility = 'visible' OR Hits > 99" data/$SEMESTER.csv | csvcut -c 1 | sed 's/\.0//' > data/$SEMESTER.txt
+csvsql --query "SELECT * FROM '$SEMESTER' WHERE Visibility = 'visible' OR Hits > 99" data/$SEMESTER.csv | csvcut -c 1 | sed 's/\.0//' | sed '/Moodle/d' | sort > data/$SEMESTER.txt
