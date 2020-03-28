6142ee193fff6ce60216b35a25d691c8a08b9f93
phette23 <phette23@gmail.com>
2020-03-28 12:34:26 -0700

    download - use rsync instead of scp
    more likely to be able to resume interrupted download

--- download.sh
+++ download.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
-scp moodle:~/mdl-backup.log data/$(date "+%Y-%m-%d").log
-scp moodle:~/backups.tar.gz backups
+rsync -vhP moodle:~/mdl-backup.log data/$(date "+%Y-%m-%d").log
+rsync -vhP moodle:~/backups.tar.gz backups
 ssh moodle 'rm -v ~/backups.tar.gz'
