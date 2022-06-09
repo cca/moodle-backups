52c260a06922894712f7ecb200b77122d5c19cdd
phette23 <phette23@gmail.com>
2022-06-09 15:46:59 -0700

    no need for unused createdb script

--- createdb.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-# create database
-csvsql --db sqlite:///courses.db --table courses --insert file.csv
-# add to already-existing database
-csvsql --db sqlite:///courses.db --table courses --no-create --insert file.csv
-# query spreadsheet and get a list of IDs, can change the semester code here
-SEMESTER="2018SP"
-csvsql --query "SELECT * FROM '$SEMESTER' WHERE Visibility = 'visible' OR Hits > 99" data/$SEMESTER.csv | csvcut -c 1 | sed 's/\.0//' | sed '/Moodle/d' | sort > data/$SEMESTER.txt
