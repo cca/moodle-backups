eb895b154a0f37ef2d7839350ec522a828183a7d
phette23 <phette23@gmail.com>
2022-06-21 12:59:26 -0700

    change rm command to only do one course at a time

--- backup.fish
+++ backup.fish
@@ -6,7 +6,7 @@ function usage -d 'describes how to use this script'
     echo -e "\t./$name mk COURSE1 [ COURSE2 COURSE3... ] - create a backup on the Moodle pod"
     echo -e "\t./$name dl [ COURSE1 COURSE2... | --all ] - download specific, or all, backup files from the Moodle pod"
     echo -e "\t./$name cp SEMESTER FILE1 [FILE2 FILE3...] - copy backup file(s) to SEMESTER folder in GSB"
-    echo -e "\t./$name rm [ COURSE1 COURSE2... | --all ] - delete specific, or all, backup files from the Moodle pod"
+    echo -e "\t./$name rm COURSE - delete the backup file and course from the Moodle pod"
 end
 
 switch $argv[1]
