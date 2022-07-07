9aa5e140a202e9ca27130e7d86e7d8c009ce0ab9
phette23 <phette23@gmail.com>
2022-07-06 17:36:57 -0700

    download.sh - unpack backups.tar after download

--- download.sh
+++ download.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/env bash
 rsync -vhP ma:~/moodle-backups/data/log.txt data/$(date "+%Y-%m-%d").log
 rsync -vhP ma:~/backups.tar data/ && ssh ma 'rm -v ~/backups.tar'
+tar xvf data/backups.tar --strip-components 4 --directory=data
