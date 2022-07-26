d4d972b9d04a89356d7991a4b320a85e1790484a
phette23 <phette23@gmail.com>
2022-07-26 11:49:30 -0700

    download.sh can rm backups.tar if it was unpacked successfully

--- download.sh
+++ download.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 rsync -vhP ma:~/moodle-backups/data/log.txt data/$(date "+%Y-%m-%d").log
 rsync -vhP ma:~/backups.tar data/ && ssh ma 'rm -v ~/backups.tar'
-tar xvf data/backups.tar --strip-components 4 --directory=data
+tar xvf data/backups.tar --strip-components 4 --directory=data && rm -v data/backups.tar
