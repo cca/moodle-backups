9011040ce2a8bcef56a0e3389e2893ffb35b34ab
phette23 <phette23@gmail.com>
2022-08-01 12:19:28 -0700

    rm old shell scripts from prior project, closes #5
    end of an era that began when the pandemic started

--- download.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-rsync -vhP ma:~/moodle-backups/data/log.txt data/$(date "+%Y-%m-%d").log
-rsync -vhP ma:~/backups.tar data/ && ssh ma 'rm -v ~/backups.tar'
-tar xvf data/backups.tar --strip-components 4 --directory=data && rm -v data/backups.tar
